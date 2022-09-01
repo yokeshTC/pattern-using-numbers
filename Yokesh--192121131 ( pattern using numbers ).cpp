3dbc8035bd8efb5a93defc7f391d42c77946a639
yokeshTC <112368654+yokeshTC@users.noreply.github.com>
2022-09-01 10:31:42 +0530

    Add files via upload

---
+++ Yokesh--192121131 ( pattern using numbers ).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rows, columns, n = 5;
+
+	for (rows = 1; rows <= n; rows++) {
+
+		for (columns = 1; columns <= rows; columns++) {
+
+			cout << rows << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
+
